df8c242ab66efcc677d59fb27354cd7cd060fa8f
abdokin <bashbin.sh@gmail.com>
2024-05-01 18:44:35 +0100

    make home page interesting

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,20 +2,25 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <Link href={"/auth/login"}>
-          <Button>Get started </Button>
-        </Link>
-        <Link href={"/auth/register"}>
-          <Button>Register </Button>
-        </Link>
 
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          Found Tracker
+  return (
+    <div className="h-screen min-h-screen container bg-gray-900 text-white">
+      <nav className="flex items-center w-full bg-white px-8">
+        <div className="bg-white p-4 text-black font-bold text-2xl">
+          <span>Lost Objects Tracker</span>
         </div>
-      </div>
-    </main>
+        <div className="flex gap-2 items-center ml-auto">
+          <Link href={"/auth/login"}>
+            <Button>Report Lost Object</Button>
+          </Link>
+          <Link href={"/auth/register"}>
+            <Button>Register</Button>
+          </Link>
+        </div>
+      </nav>
+      <main className="flex flex-col items-center justify-between  text-white mt-8">
+          <h1 className="text-3xl font-bold">Welcome To Système de Gestion des Objets Trouvés au FSAC </h1>
+      </main>
+    </div>
   );
 }
