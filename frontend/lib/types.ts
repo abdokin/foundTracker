12ff1fbf6198b43b84a4d99d039540151ccb59be
abdokin <bashbin.sh@gmail.com>
2024-05-02 19:00:04 +0100

    add more types

--- frontend/lib/types.ts
+++ frontend/lib/types.ts
@@ -8,6 +8,26 @@ export type RegisterInput = {
   firstname: string;
   lastname: string;
 };
+
+
+
+export type ResetPasswordInput = {
+  currentPassword: string,
+  newPassword: string,
+  confirmationPassword: string
+}
+
+export type AddItemInput = {
+  name: string,
+  description: string;
+  images: File[];
+}
+export type EditProfileInput = {
+  id: string,
+  firstname: string,
+  lastname: string,
+  email: string,
+}
 export type ApiResponse<T> = {
   success: boolean;
   message: string;
@@ -34,4 +54,57 @@ export type AuthResponse = {
   access_token: string;
   refresh_token: string;
   user: User;
+};
+
+
+export type Image = {
+  id: number;
+  imageUrl: string;
+  item: null;
+};
+
+export type Item = {
+  id: number;
+  name: string;
+  description: string;
+  foundDateTime: string;
+  images: Image[];
+};
+
+export type Pagination = {
+  pageNumber: number;
+  pageSize: number;
+}
+
+export type Pageable = {
+  pageNumber: number;
+  pageSize: number;
+  sort: {
+      empty: boolean;
+      sorted: boolean;
+      unsorted: boolean;
+  };
+  offset: number;
+  paged: boolean;
+  unpaged: boolean;
+};
+
+
+
+export type Page<T> = {
+  content: T[];
+  pageable: Pageable;
+  totalPages: number;
+  totalElements: number;
+  last: boolean;
+  size: number;
+  number: number;
+  sort: {
+      empty: boolean;
+      sorted: boolean;
+      unsorted: boolean;
+  };
+  numberOfElements: number;
+  first: boolean;
+  empty: boolean;
 };
