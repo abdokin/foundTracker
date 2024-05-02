833e9fe4cd96e386620468e940d044b0f41b7368
abdokin <bashbin.sh@gmail.com>
2024-05-02 18:55:06 +0100

    add univ logo

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,10 +4,11 @@ import Link from "next/link";
 export default function Home() {
 
   return (
-    <div className="h-screen min-h-screen container bg-gray-900 text-white">
-      <nav className="flex items-center w-full bg-white px-8">
+    <div className="h-screen min-h-screen container  text-white">
+      <nav className="flex items-center w-full bg-white px-8 border-b">
         <div className="bg-white p-4 text-black font-bold text-2xl">
-          <span>Lost Objects Tracker</span>
+          {/* <span>Lost Objects Tracker</span> */}
+          <img src="/logo.png" alt="" width={120} height={120} />
         </div>
         <div className="flex gap-2 items-center ml-auto">
           <Link href={"/auth/login"}>
@@ -18,8 +19,8 @@ export default function Home() {
           </Link>
         </div>
       </nav>
-      <main className="flex flex-col items-center justify-between  text-white mt-8">
-          <h1 className="text-3xl font-bold">Welcome To Système de Gestion des Objets Trouvés au FSAC </h1>
+      <main className="flex flex-col items-center justify-between  text-black mt-8">
+        <h1 className="text-3xl font-bold">Welcome To Système de Gestion des Objets Trouvés au FSAC </h1>
       </main>
     </div>
   );
