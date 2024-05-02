5fc18b2c407f1e66d8a7572749e19b812c17882a
abdokin <bashbin.sh@gmail.com>
2024-05-02 18:57:23 +0100

    NavBar Component

---
+++ frontend/components/nav-bar.tsx
@@ -0,0 +1,69 @@
+"use client"
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { Menu, Search } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { UserNav } from "@/components/user-nav";
+import { User } from "@/lib/types";
+import { usePathname } from "next/navigation";
+
+export default function NavBar({ user }: { user: User }) {
+    const path = usePathname();
+
+    const links = [
+        // { path: "/", label: "FoundTracker" },
+        { path: "/dashboard", label: "Dashboard" },
+        { path: "/dashboard/items", label: "Items" },
+        { path: "/dashboard/reclamations", label: "Reclamations" },
+    ];
+
+    return (
+        <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+            <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+                {links.map((link) => (
+                    <Link key={link.path} href={link.path} className={path === link.path ? "text-foreground" : "text-muted-foreground transition-colors hover:text-foreground"}>
+                        {link.label}
+                    </Link>
+                ))}
+            </nav>
+            <Sheet>
+                <SheetTrigger asChild>
+                    <Button
+                        variant="outline"
+                        size="icon"
+                        className="shrink-0 md:hidden"
+                    >
+                        <Menu className="h-5 w-5" />
+                        <span className="sr-only">Toggle navigation menu</span>
+                    </Button>
+                </SheetTrigger>
+                <SheetContent side="left">
+                    <nav className="grid gap-6 text-lg font-medium">
+                        {links.map((link) => (
+                            <Link key={link.path} href={link.path} className={path === link.path ? "text-foreground" : "text-muted-foreground transition-colors hover:text-foreground"}>
+                                {link.label}
+
+                            </Link>
+                        ))}
+                    </nav>
+                </SheetContent>
+            </Sheet>
+            <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+                <form className="ml-auto flex-1 sm:flex-initial">
+                    <div className="relative">
+                        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                        <Input
+                            type="search"
+                            placeholder="Search objets..."
+                            className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+                        />
+                    </div>
+                </form>
+                <UserNav user={user} />
+            </div>
+        </header>
+    );
+}
