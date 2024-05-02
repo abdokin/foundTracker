b99433f4ee106d6db5b49dd40a25f3019bde7797
abdokin <bashbin.sh@gmail.com>
2024-05-02 19:01:01 +0100

    add notBlank validations

--- src/main/java/com/foundtracker/web/dto/CreateItemDto.java
+++ src/main/java/com/foundtracker/web/dto/CreateItemDto.java
@@ -2,6 +2,8 @@ package com.foundtracker.web.dto;
 
 import com.foundtracker.web.model.Item;
 import com.foundtracker.web.validator.ImageFiles;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
@@ -15,13 +17,17 @@ import java.util.List;
 @Data
 public class CreateItemDto {
     @NotNull(message = "Name cannot be null")
+    @NotBlank(message = "Name cannot be empty")
     @Size(min = 1, max = 255, message = "Name length must be between 1 and 255 characters")
     private String name;
 
     @NotNull(message = "Description cannot be null")
+    @NotBlank(message = "Description cannot be empty")
     @Size(max = 1000, message = "Description length cannot exceed 1000 characters")
     private String description;
 
+    @NotNull(message = "Images cannot be null")
+    @Size(min = 1, message = "Images cannot be empty")
     @ImageFiles
     List<MultipartFile> images;
     public  static Item mapToItem(CreateItemDto createItemDto) {
