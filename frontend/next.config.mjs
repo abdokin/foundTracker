3de4c4be9a45650d1a1c3a1b37c83c37a13fd53e
abdokin <bashbin.sh@gmail.com>
2024-05-02 18:55:29 +0100

    allow localhost and increase bodySizeLimit

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: 'localhost',
+            },
+        ],
+    },
+    experimental: {
+        serverActions: {
+          bodySizeLimit: '10mb',
+        },
+      },
+
+};
 
 export default nextConfig;
