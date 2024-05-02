8e5a5bbc16fc7160324755d95587b58d5e7f51a0
abdokin <bashbin.sh@gmail.com>
2024-05-02 19:01:14 +0100

    edit profileDTO class

---
+++ src/main/java/com/foundtracker/web/dto/EditProfileDto.java
@@ -0,0 +1,27 @@
+package com.foundtracker.web.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(name = "EditProfileInput")
+public class EditProfileDto {
+    private  int id;
+    @NotBlank(message = "First name is required")
+    private String firstname;
+    @NotBlank(message = "Last name is required")
+    private String lastname;
+    @NotEmpty(message = "email is required")
+    @Email
+    private String email;
+
+}
