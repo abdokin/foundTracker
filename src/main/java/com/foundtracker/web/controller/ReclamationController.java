9a8da3af8c73f2fbd5ad1aeba9b9bdf44e2b5011
abdokin <bashbin.sh@gmail.com>
2024-05-03 22:08:22 +0100

    Reclamation Controller

--- src/main/java/com/foundtracker/web/controller/ReclamationController.java
+++ src/main/java/com/foundtracker/web/controller/ReclamationController.java
@@ -1,2 +1,40 @@
-package com.foundtracker.web.controller;public class ReclamationController {
+package com.foundtracker.web.controller;
+
+
+import com.foundtracker.web.dto.ReclamationDto;
+import com.foundtracker.web.responses.ApiResponse;
+import com.foundtracker.web.service.ReclamationService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/reclamations")
+@RequiredArgsConstructor
+@Tag(name = "Reclamations Management")
+public class ReclamationController {
+    private final ReclamationService reclamationService;
+
+    @GetMapping
+    public ApiResponse<Page<ReclamationDto>> getAllItems(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        Page<ReclamationDto> reclamations =reclamationService.findAll(PageRequest.of(page, size));
+
+        return ApiResponse.success(reclamations,"Reclamations loaded successfully");
+    }
+    @GetMapping("/{reclamationId}")
+    public ApiResponse<ReclamationDto> getReclamation(@PathVariable Long reclamationId) {
+        ReclamationDto reclamation = reclamationService.findById(reclamationId);
+        return ApiResponse.success(reclamation,"Reclamation loaded successfully");
+    }
+
+    @PostMapping("/{itemId}")
+    private ApiResponse<ReclamationDto> create(@PathVariable long itemId) {
+        ReclamationDto reclamation = reclamationService.save(itemId);
+        return ApiResponse.success(reclamation,"Reclamation created successfully");
+    }
 }
