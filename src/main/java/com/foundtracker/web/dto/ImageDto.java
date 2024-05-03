77619d194b575d9cff046f028c1a586a80e53697
abdokin <bashbin.sh@gmail.com>
2024-05-03 22:08:56 +0100

    Image DTO

--- src/main/java/com/foundtracker/web/dto/ImageDto.java
+++ src/main/java/com/foundtracker/web/dto/ImageDto.java
@@ -1,2 +1,24 @@
-package com.foundtracker.web.dto;public class ImageDto {
+package com.foundtracker.web.dto;
+
+import com.foundtracker.web.model.Image;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Schema(name = "Image")
+public class ImageDto {
+    private Long id;
+    private String imageUrl;
+    public static ImageDto mapToDto(Image image) {
+        return ImageDto.builder()
+                .id(image.getId())
+                .imageUrl(image.getImageUrl())
+                .build();
+    }
 }
