5defc57967cffc06c602c88964e0b1379ffdec15
abdokin <bashbin.sh@gmail.com>
2024-05-03 22:09:09 +0100

    Notification DTO

--- src/main/java/com/foundtracker/web/dto/NotificationDto.java
+++ src/main/java/com/foundtracker/web/dto/NotificationDto.java
@@ -1,2 +1,29 @@
-package com.foundtracker.web.dto;public class NotificationDto {
+package com.foundtracker.web.dto;
+
+import com.foundtracker.web.model.Notification;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@Schema(name = "Notification")
+public class NotificationDto {
+    private int id;
+    private String message;
+    private LocalDateTime receivedAt;
+    private ReclamationDto reclamationDto;
+    public  static NotificationDto mapToDto(Notification notification){
+        return NotificationDto.builder()
+                .id(notification.getId())
+                .message(notification.getMessage())
+                .receivedAt(notification.getReceivedAt())
+                .reclamationDto(ReclamationDto.mapToDto(notification.getReclamation()))
+                .build();
+    }
 }
