dc98f928f6c62e234dc46d2fca774b38ba7bb0e8
abdokin <bashbin.sh@gmail.com>
2024-05-03 22:10:25 +0100

    Clean Up

--- src/main/java/com/foundtracker/web/model/Image.java
+++ src/main/java/com/foundtracker/web/model/Image.java
@@ -3,6 +3,10 @@ package com.foundtracker.web.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.Instant;
 
 
 @Data
@@ -15,10 +19,11 @@ public class Image {
     @Id
     @GeneratedValue
     private Long id;
-
+    @Column(nullable = false)
     private String imageUrl;
 
     @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.REMOVE)
     @JoinColumn(name = "item_id", nullable = false)
     private Item item;
+
 }
