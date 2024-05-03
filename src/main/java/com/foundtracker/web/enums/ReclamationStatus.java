2cdfa5c63babd6a7ba5d71bd8bf2dd756c9f361e
abdokin <bashbin.sh@gmail.com>
2024-05-03 22:10:16 +0100

    Reclamation Status

--- src/main/java/com/foundtracker/web/enums/ReclamationStatus.java
+++ src/main/java/com/foundtracker/web/enums/ReclamationStatus.java
@@ -1,2 +1,18 @@
-package com.foundtracker.web.enums;public class ReclamationStatus {
+package com.foundtracker.web.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum ReclamationStatus {
+    PENDING("PENDING"),
+    APPROVED("APPROVED"),
+    REJECTED("REJECTED"),
+    COMPLETED("COMPLETED"),
+    CANCELLED("CANCELLED");
+
+    private final String status;
+
+     ReclamationStatus(String status) {
+        this.status = status;
+    }
 }
