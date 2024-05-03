d775af5aefbb024fa313ad86a6681e97ce900458
abdokin <bashbin.sh@gmail.com>
2024-05-03 22:07:29 +0100

    AuditorAware Implementation

--- src/main/java/com/foundtracker/web/config/AuditorAwareImpl.java
+++ src/main/java/com/foundtracker/web/config/AuditorAwareImpl.java
@@ -1,2 +1,23 @@
-package com.foundtracker.web.config;public class AuditorAwareImpl {
+package com.foundtracker.web.config;
+
+import com.foundtracker.web.model.User;
+import io.micrometer.common.lang.NonNullApi;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class AuditorAwareImpl implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getPrincipal)
+                .map(User.class::cast)
+                .map(User::getUsername)
+                ;
+    }
 }
