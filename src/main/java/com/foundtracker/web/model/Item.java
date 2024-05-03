d8e0c82ab22322628e6647804525dea02aebad33
abdokin <bashbin.sh@gmail.com>
2024-05-03 22:10:36 +0100

    clean up

--- src/main/java/com/foundtracker/web/model/Item.java
+++ src/main/java/com/foundtracker/web/model/Item.java
@@ -1,8 +1,12 @@
 package com.foundtracker.web.model;
 
+import com.foundtracker.web.enums.ItemStatus;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -17,10 +21,19 @@ public class Item {
     @Id
     @GeneratedValue
     private Integer id;
+    @Column(nullable = false)
     String name;
+    @Column(nullable = false,length = 300) //MAX Len is 300
     String description;
-
     private LocalDateTime foundDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private ItemStatus status = ItemStatus.FOUND;
     @OneToMany(mappedBy = "item", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Image> images;
+
+    @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "user_id")
+    public User user;
+
 }
