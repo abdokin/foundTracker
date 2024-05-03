496a0b22eac11f91cdedb796aeb8019cb5795fcb
abdokin <bashbin.sh@gmail.com>
2024-05-03 22:12:08 +0100

    Notification Repository

--- src/main/java/com/foundtracker/web/repository/NotificationRepository.java
+++ src/main/java/com/foundtracker/web/repository/NotificationRepository.java
@@ -1,2 +1,13 @@
-package com.foundtracker.web.repository;public class NotificationRepository {
+package com.foundtracker.web.repository;
+
+import com.foundtracker.web.model.Notification;
+import com.foundtracker.web.model.User;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+    public Page<Notification> findAllByUser(User user, Pageable pageable);
+    public Page<Notification> findByUserAndReclamationId(User user, long reclamation_id, Pageable pageable);
 }
