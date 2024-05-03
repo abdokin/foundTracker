333a065b979b6eb7f6eca6e78eecff84e3820355
abdokin <bashbin.sh@gmail.com>
2024-05-03 22:07:38 +0100

    WebsocketBroker Config

--- src/main/java/com/foundtracker/web/config/WebSocketBrokerConfig.java
+++ src/main/java/com/foundtracker/web/config/WebSocketBrokerConfig.java
@@ -1,2 +1,22 @@
-package com.foundtracker.web.config;public class WebSocketBrokerConfig {
+package com.foundtracker.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketBrokerConfig implements WebSocketMessageBrokerConfigurer {
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/notification");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
 }
