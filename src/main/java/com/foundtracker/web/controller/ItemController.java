7fac5632dfe36ebf6fcd17b4ef6e97ce1c11105b
abdokin <bashbin.sh@gmail.com>
2024-05-03 22:07:50 +0100

    clean up

--- src/main/java/com/foundtracker/web/controller/ItemController.java
+++ src/main/java/com/foundtracker/web/controller/ItemController.java
@@ -3,6 +3,7 @@ package com.foundtracker.web.controller;
 
 import com.foundtracker.web.dto.CreateItemDto;
 import com.foundtracker.web.dto.EditItemDto;
+import com.foundtracker.web.dto.ItemDto;
 import com.foundtracker.web.model.Item;
 import com.foundtracker.web.responses.ApiResponse;
 import com.foundtracker.web.service.ItemService;
@@ -17,28 +18,36 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/v1/management/items")
+@RequestMapping("/api/v1/items")
 @RequiredArgsConstructor
-@Tag(name = "Management")
+@Tag(name = "Items")
 public class ItemController {
     private final ItemService itemService;
 
     @GetMapping
-    public ApiResponse<Page<Item>> getAllItems(
+    public ApiResponse<Page<ItemDto>> getAllItems(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size
     ) {
-        Page<Item> items = itemService.findAll(PageRequest.of(page, size));
+        Page<ItemDto> items = itemService.findAll(PageRequest.of(page, size));
 
         return ApiResponse.success(items,"items loaded successfully");
     }
 
-    @PostMapping(value = "/create", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
-    public ApiResponse<Item> addItem(@ModelAttribute @Valid CreateItemDto item) throws IOException {
+    @PostMapping(value = "/create", consumes = {
+            MediaType.MULTIPART_FORM_DATA_VALUE,
+            MediaType.APPLICATION_JSON_VALUE
+    })
+    public ApiResponse<ItemDto> addItem(@ModelAttribute @Valid CreateItemDto item) throws IOException {
         return ApiResponse.success(itemService.save(item),"item added successfully");
     }
-        @PatchMapping(value = "/{id}/edit",consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
-    public ApiResponse<Item> editItem(@PathVariable Long id, @ModelAttribute @Valid EditItemDto itemDto) {
+
+    @PatchMapping(value = "/{id}/edit", consumes = {
+            MediaType.MULTIPART_FORM_DATA_VALUE,
+            MediaType.APPLICATION_JSON_VALUE
+    })
+    public ApiResponse<?> editItem(@PathVariable Long id,
+                                   @ModelAttribute @Valid EditItemDto itemDto) {
         return ApiResponse.success(itemService.edit(id, itemDto), "Item edited successfully");
     }
 }
