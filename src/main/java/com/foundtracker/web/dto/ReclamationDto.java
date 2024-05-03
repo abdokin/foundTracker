9789dabdbcd85508aece1af8460542a04cb4a7f9
abdokin <bashbin.sh@gmail.com>
2024-05-03 22:09:21 +0100

    Reclamation DTO

--- src/main/java/com/foundtracker/web/dto/ReclamationDto.java
+++ src/main/java/com/foundtracker/web/dto/ReclamationDto.java
@@ -1,2 +1,31 @@
-package com.foundtracker.web.dto;public class ReclamationDto {
+package com.foundtracker.web.dto;
+
+import com.foundtracker.web.enums.ReclamationStatus;
+
+import com.foundtracker.web.model.Reclamation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(name = "Reclamation")
+public class ReclamationDto {
+    private long id;
+    private ReclamationStatus status = ReclamationStatus.PENDING;
+    private ItemDto item;
+    private UserDto user;
+
+    public static ReclamationDto mapToDto(Reclamation reclamation) {
+        return ReclamationDto.builder()
+                .id(reclamation.getId())
+                .status(reclamation.getStatus())
+                .item(ItemDto.mapToDto(reclamation.getItem()))
+                .user(UserDto.mapToUserDto(reclamation.getUser()))
+                .build();
+    }
 }
