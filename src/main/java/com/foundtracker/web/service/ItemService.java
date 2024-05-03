01494bfc7658d01cac675bec7783eccc51191418
abdokin <bashbin.sh@gmail.com>
2024-05-03 22:12:53 +0100

    clean up

--- src/main/java/com/foundtracker/web/service/ItemService.java
+++ src/main/java/com/foundtracker/web/service/ItemService.java
@@ -3,9 +3,12 @@ package com.foundtracker.web.service;
 
 import com.foundtracker.web.dto.CreateItemDto;
 import com.foundtracker.web.dto.EditItemDto;
+import com.foundtracker.web.dto.ItemDto;
+import com.foundtracker.web.enums.ItemStatus;
 import com.foundtracker.web.exception.ItemNotFoundException;
 import com.foundtracker.web.model.Image;
 import com.foundtracker.web.model.Item;
+import com.foundtracker.web.model.User;
 import com.foundtracker.web.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -14,7 +17,6 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -22,33 +24,34 @@ public class ItemService {
 
     private final ItemRepository itemRepository;
     private  final ImageService imageService;
-    public Item save(CreateItemDto input) throws IOException {
-        Item item = CreateItemDto.mapToItem(input);
+    private final UserService userService;
+
+    public ItemDto save(CreateItemDto input) throws IOException,IllegalStateException {
+        Item item = CreateItemDto.mapToDto(input);
+        item.setStatus(ItemStatus.FOUND);
+        item.setUser(userService.getCurrentUser());
         itemRepository.save(item);
         List<Image> images = imageService.storeImages(input.getImages(),item);
         item.setImages(images);
-        return item;
+        return ItemDto.mapToDto(item);
     }
-    public Page<Item> findAll(Pageable pageable) {
+    public Page<ItemDto> findAll(Pageable pageable) {
         Page<Item> items = itemRepository.findAll(pageable);
-        items.map(item -> {
-            item.setImages(item.getImages().stream().peek(image -> image.setItem(null)).collect(Collectors.toList()));
-            return item;
-        });
-        return items;
+        return items.<ItemDto>map(ItemDto::mapToDto);
     }
-    public Item findById(Long id) {
-        return itemRepository.findById(id).orElse(null);
+    public ItemDto findById(Long id) {
+        Item item = itemRepository.findById(id).orElse(null);
+        assert item != null;
+        return ItemDto.mapToDto(item);
     }
     public void delete(Long id) {
         itemRepository.deleteById(id);
     }
-    public Item edit(Long id, EditItemDto input) {
+    public ItemDto edit(Long id, EditItemDto input) {
         Item item = itemRepository.findById(id)
                 .orElseThrow(() -> new ItemNotFoundException(id));
-
         item.setName(input.getName());
         item.setDescription(input.getDescription());
-        return itemRepository.save(item);
+        return ItemDto.mapToDto(itemRepository.save(item));
     }
 }
