eaf40cdd6a44561bfe41e250cb44ebe09adf2a8b
abdokin <bashbin.sh@gmail.com>
2024-05-03 22:08:38 +0100

    clean up

--- src/main/java/com/foundtracker/web/dto/CreateItemDto.java
+++ src/main/java/com/foundtracker/web/dto/CreateItemDto.java
@@ -2,12 +2,12 @@ package com.foundtracker.web.dto;
 
 import com.foundtracker.web.model.Item;
 import com.foundtracker.web.validator.ImageFiles;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
+import org.hibernate.validator.constraints.UniqueElements;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
@@ -19,6 +19,7 @@ public class CreateItemDto {
     @NotNull(message = "Name cannot be null")
     @NotBlank(message = "Name cannot be empty")
     @Size(min = 1, max = 255, message = "Name length must be between 1 and 255 characters")
+    @UniqueElements(message = "Name must be Unique")
     private String name;
 
     @NotNull(message = "Description cannot be null")
@@ -30,7 +31,7 @@ public class CreateItemDto {
     @Size(min = 1, message = "Images cannot be empty")
     @ImageFiles
     List<MultipartFile> images;
-    public  static Item mapToItem(CreateItemDto createItemDto) {
+    public  static Item mapToDto(CreateItemDto createItemDto) {
         return Item.builder()
                 .name(createItemDto.name)
                 .description(createItemDto.description)
