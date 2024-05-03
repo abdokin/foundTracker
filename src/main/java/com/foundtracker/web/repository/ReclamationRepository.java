d1905d32faed92141342b30ff08717b3d9a8e3b5
abdokin <bashbin.sh@gmail.com>
2024-05-03 22:12:18 +0100

    ReclamationRepo

--- src/main/java/com/foundtracker/web/repository/ReclamationRepository.java
+++ src/main/java/com/foundtracker/web/repository/ReclamationRepository.java
@@ -1,2 +1,14 @@
-package com.foundtracker.web.repository;public class ReclamationRepository {
+package com.foundtracker.web.repository;
+
+import com.foundtracker.web.model.Reclamation;
+import com.foundtracker.web.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ReclamationRepository extends JpaRepository<Reclamation, Long> {
+    Page<Reclamation> findAllByUser(User user, Pageable pageable);
+    Optional<Reclamation> findByUserAndId(User user, Long id);
 }
